6ab616ef3c667985e780420dfcde50ded8c4b505
Aaron I Trierweiler <ait530@users.noreply.github.com>
2019-09-29 17:14:24 -0400

    JSdoc branch initial commit; add comments to title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,6 +4,10 @@ interface IProps {
   title: string;
 }
 
+/**
+ * @class Title
+ * @description Handles the headline of document pages
+ */
 export class Title extends React.Component<IProps> {
 
   public render(): JSX.Element {
